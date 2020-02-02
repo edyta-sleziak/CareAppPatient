7ecb44cfa138426b4b78c417e6d76eaaa2939e76
LAPTOP-QQULPCJ7\edyta <edyta.sleziak@gmail.com>
2020-02-02 17:51:29 +0000

    Main activity layout added and stubs for buttons

--- app/src/main/java/org/wit/careapp/MainActivity.kt
+++ app/src/main/java/org/wit/careapp/MainActivity.kt
@@ -2,11 +2,31 @@ package org.wit.careapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        SOS.setOnClickListener {
+            toast("You clicked SOS option")
+        }
+
+        callEmergencyNumber.setOnClickListener {
+            toast("You clicked CALL option")
+        }
+
+        Notes.setOnClickListener {
+            toast("You clicked NOTES option")
+        }
+
+        ToDo.setOnClickListener {
+            toast("You clicked TODO LIST option")
+        }
     }
+
+
 }
